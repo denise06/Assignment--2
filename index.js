4e1c5521f8561eb0b128217bf906652812bee289
denise06 <dchan008@e.ntu.edu.sg>
2021-09-14 15:03:14 +0000

    use of restful API, remove hbs, wax on etc.

--- index.js
+++ index.js
@@ -8,25 +8,18 @@ const ObjectId = require('mongodb').ObjectId;
 // setup environmental variables to store the mongo connection string
 require('dotenv').config();
 
-let app = express()
-app.set('view engine', 'hbs')
+let app = express();
+// app.set('view engine', 'hbs')
 
 
 // define static images in images folder
 app.use(express.static('images'))
-// set up wax on 
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts')
+// // set up wax on 
 
-// const helpers = require('handlebars-helpers');
-// helpers({
-//     'handlebars': hbs.handlebars
-// })
-
-app.use(express.urlencoded({
-    'extended': false
-}))
-app.use (express.json)
+// app.use(express.urlencoded({
+//     'extended': false
+// }))
+app.use (express.json());
 app.use(cors())
 
 
@@ -35,35 +28,11 @@ app.use(cors())
 async function main() {
     await MongoUtil.connect(process.env.MONGO_URL,"homebakers");
 
-    
-    // // Homepage - all item view
-    // app.get('/item_record', async function (req, res) {
-    //     let db = MongoUtil.getDB();
-    //     let itemRecords = await db.collection('listings').find({}).project({
-    //         'itemName': 1,
-    //         'itemDesc': 1,
-    //         'itemPrice':1,
-    //         'contactInfo':1,
-    //         'category':1
-    //     }).toArray();
-    //         res.render("item", {
-    //             'itemRecords': itemRecords
-    //         });
-    // })
-
-    // // Create new listing via form
-    // app.get('/item_record/create', function (req, res) {
-    //     res.render('create_item')
-    // })
-
-
-    // Process the form
-
     app.post('/item_record', function (req, res) {
-        let item_name = req.body.item_name;
-        let item_desc = req.body.item_description;
-        let item_price = req.body.item_price
-        let contact_info = req.body.contact_info
+        let itemName = req.body.itemName;
+        let itemDesc = req.body.itemDesc;
+        let itemPrice = req.body.itemPrice
+        let contactInfo = req.body.contactInfo
         let category = req.body.category;
         console.log(req.body);
         if (!category){
@@ -74,73 +43,80 @@ async function main() {
         
         let db = MongoUtil.getDB();
         db.collection('listings').insertOne({
-            'itemName': item_name,
-            'itemDesc': item_desc,
-            'itemPrice': item_price,
-            'contactInfo': contact_info
+            'itemName': itemName,
+            'itemDesc': itemDesc,
+            'itemPrice': itemPrice,
+            'contactInfo': contactInfo,
+            'category': category
         })
         res.status(200);
         res.json({
             'insertedId': result.insertedID
         })
-
-        // res.redirect('/item_record')
     });
+    
+    app.get('/item_record', async function(req,res){
+        let db = MongoUtil.getDB();
+        let results = await db.collection('listings').find({}).toArray();
+        res.json(results);
+    })
+
 
     // Edit item listing
-    app.put('/:itemID/update', async function (req, res) {
+    app.put('item_record/:itemId', async function (req, res) {
         let db = MongoUtil.getDB();
-        let itemID = req.params.itemID;
-        // let response = await axios.get ('')
-        res.render('edit_item', {
-            'listings': response.data
-        })
-    })
-
-    app.post('/:itemID/update', async function (req, res) {
-        let itemID = req.params.itemID;
-        // let item = {
-        //     "item_id": Math.floor(Math.random()*1000000+ 10000),
-        //     "item_name": req.body.item_name,
-        //     "item_desc": req.body.item_description,
-        //     "category":{
-        //         "cat_id": 1,
-        //         "name": req.body.category
-        //     },
-        //     "contact_info": "number"
-        // }
-        res.redirect('/item')
-    })
-
-    // Adding review to an item page (subdoc)
-    app.get('/item_record/:itemID/reviews/add', async function (req, res) {
-        let db = MongoUtil.getDB();
-        let itemDetails = await db.collection('listings').findOne({
-            '_id': ObjectId(req.params.id)
-        })
-        res.render('add_review', {
-            'item': itemDetails
-        })
-    })
-
-
-
-    app.post('/item_record/:item/reviews/add', async function (req, res) {
-        let db = MongoUtil.getDB();
-        let reviewContent = req.body.content;
-        let id = req.params.id;
-        let response = await db.collection('homebakers').updateOne({
-            '_id': ObjectId(id)
+        let results = await db.collection('food').updateOne({
+            '_id':ObjectId(req.params.itemId)
         }, {
-            '$push': {
-                'notes': {
-                    '_id': ObjectId(),
-                    'review': reviewContent
-                }
+            '$set': {
+                'itemName': itemName,
+                'itemDesc': itemDesc,
+                'itemPrice': itemPrice,
+                'contactInfo': contactInfo,
+                'category': category
             }
         })
+        res.json(results);
         res.redirect('/item_record')
     })
+
+    app.delete('/item_record/:itemId', async function(req,res){
+        let db = MongoUtil.getDB();
+        let results = await db.collection('listing').deleteOne({
+            "_id": ObjectId(req.params.itemId)
+        })
+        res.json(results);
+        res.redirect('/item_record')
+    })
+
+
+    // // Adding review to an item page (subdoc)
+    // app.get('/item_record/:itemID/reviews/add', async function (req, res) {
+    //     let db = MongoUtil.getDB();
+    //     let itemDetails = await db.collection('listings').findOne({
+    //         '_id': ObjectId(req.params.id)
+    //     })
+    //     res.render('add_review', {
+    //         'item': itemDetails
+    //     })
+    // })
+
+    // app.post('/item_record/:item/reviews/add', async function (req, res) {
+    //     let db = MongoUtil.getDB();
+    //     let reviewContent = req.body.content;
+    //     let id = req.params.id;
+    //     let response = await db.collection('homebakers').updateOne({
+    //         '_id': ObjectId(id)
+    //     }, {
+    //         '$push': {
+    //             'notes': {
+    //                 '_id': ObjectId(),
+    //                 'review': reviewContent
+    //             }
+    //         }
+    //     })
+    //     res.redirect('/item_record')
+    // })
 }
 main();
 
